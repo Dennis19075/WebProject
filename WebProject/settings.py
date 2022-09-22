67efe3fd082f87432700dcd16973aeda34db5705
DennisChicaizaA <dennis.chicaiza.a@gmail.com>
2022-09-21 22:28:04 -0500

    Feat: . Added contact form to the website

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -128,3 +128,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
+
+#Email configuration
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="dennis.chicaiza.a@gmail.com"
+EMAIL_HOST_PASSWORD="deNnIs.chicAi7a.a"
--- contacto/views.py
+++ contacto/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from ast import If
+from django.shortcuts import render, redirect
 
 from .forms import ContactForm
+
+from django.core.mail  import EmailMessage
 
 # Create your views here.
 def contacto(request):
     form = ContactForm()
+
+    if(request.method=="POST"):
+        form=ContactForm(data=request.POST)
+        if(form.is_valid()):
+            name = request.POST.get("name")
+            email = request.POST.get("email")
+            content = request.POST.get("content")
+
+            emailMes = EmailMessage(
+                "Message from App Django",
+                "The user {} with email {} wrote this message for you:\n\n {} ".format(name,email,content), "", ["dennis.chicaiza.a@gmail.com"], reply_to=[email])
+            try:
+                emailMes.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?valido")
 
     return render(request, "contacto/contacto.html", {'form': form})
